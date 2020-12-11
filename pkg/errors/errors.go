1c633c207295b8c6d62477fc740cdaefdb8c8730
Pritam Singh <pkspritam16@gmail.com>
2020-12-11 23:13:58 +0530

    added status code to error pkg
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -40,3 +40,58 @@ func NewMsgf(status int, format string, msg ...interface{}) *Er {
 		Err:    fmt.Errorf(format, msg...),
 	}
 }
+// Status codes
+const (
+	// OK Return on Success
+	OK = iota
+	// CANCELLED The operation was cancelled, typically by the caller.
+
+	CANCELLED
+	// UNKNOWN For example, this error may be returned when a Status value received from another address space
+	// belongs to an error-space that isn't known in this address space. Also errors raised by APIs that don't
+	// return enough error information may be converted to this error.
+
+	UNKNOWN
+	// INVALID_ARGUMENT the client specified an invalid argument.
+
+	INVALID_ARGUMENT
+	// DEADLINE_EXCEEDED The deadline expired before the operation could complete.
+
+	DEADLINE_EXCEEDED
+	// NOT_FOUND Some requested entity was not found.
+
+	NOT_FOUND
+	// ALREADY_EXISTS The entity that a client attempted to create already exists.
+
+	ALREADY_EXISTS
+	// PERMISSION_DENIED The caller does not have permission to execute the specified operation.
+
+	PERMISSION_DENIED
+	// RESOURCE_EXHAUSTED Some resource has been exhausted.
+
+	RESOURCE_EXHAUSTED
+	// FAILED_PRECONDITION The operation was rejected because the system is not in a state required for the operation’s execution.
+
+	FAILED_PRECONDITION
+	// ABORTED The operation was aborted.
+
+	ABORTED
+	// OUT_OF_RANGE The operation was attempted past the valid range.
+
+	OUT_OF_RANGE
+	// UNIMPLEMENTED The operation is not implemented or is not supported/enabled in this service.
+
+	UNIMPLEMENTED
+	// INTERNAL Internal errors.
+
+	INTERNAL
+	// UNAVAILABLE The service is currently unavailable.
+
+	UNAVAILABLE
+	// DATA_LOSS Unrecoverable data loss or corruption.
+
+	DATA_LOSS
+	// UNAUTHENTICATED The request does not have valid authentication credentials for the operation.
+	UNAUTHENTICATED
+)
+
